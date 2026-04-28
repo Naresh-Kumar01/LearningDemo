18f8cd1b7e4db161b858b5ec563f18de0521b438
Naresh-Kumar01 <nareshsofttechh@gmail.com>
2026-04-28 10:26:03 -0700

    Added Day 38 Selenium screenshots and ChromeOptions concepts

--- src/day30/HandleAlertusingExplicitwait.java
+++ src/day30/HandleAlertusingExplicitwait.java
@@ -1,5 +1,37 @@
 package day30;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HandleAlertusingExplicitwait {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        // Explicit wait declaration
+        WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.manage().window().maximize();
+
+        // Opens alert box
+        driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
+
+        Thread.sleep(5000);
+
+        // Capture alert box using explicit wait
+        Alert myalert = mywait.until(ExpectedConditions.alertIsPresent());
+
+        // Accept alert
+        myalert.accept();
+
+        driver.quit();
+    }
 }
--- src/day37/FileUpload.java
+++ src/day37/FileUpload.java
@@ -1,5 +1,64 @@
 package day37;
 
-public class FileUpload {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.LocalFileDetector; // Import zaroori hai
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class FileUpload {
+    public static void main(String[] args) throws MalformedURLException {
+        
+        URL gridUrl = new URL("http://localhost:4444");
+        ChromeOptions options = new ChromeOptions();
+        
+        // WebDriver ko RemoteWebDriver class mein cast karke use karein taaki setFileDetector mil sake
+        RemoteWebDriver driver = new RemoteWebDriver(gridUrl, options);
+        
+        // CRITICAL: Yeh line aapki local file ko Docker container mein transfer karegi
+        driver.setFileDetector(new LocalFileDetector());
+        
+        try {
+            driver.get("https://davidwalsh.name/demo/multiple-file-upload.php");
+            driver.manage().window().maximize();
+            
+            // Multiple files ke absolute paths
+            String file1 = "C:\\AutomationProjects\\SeleniumFramework\\Test1.txt";
+            String file2 = "C:\\AutomationProjects\\SeleniumFramework\\Test2.txt";
+            
+            // SendKeys mein files ko "\n" se join karein
+            driver.findElement(By.xpath("//input[@id='filesToUpload']")).sendKeys(file1 + "\n" + file2);
+            
+            // Validation 1: Number of files
+            List<WebElement> uploadedFiles = driver.findElements(By.xpath("//ul[@id='fileList']//li"));
+            int noOfFilesUploaded = uploadedFiles.size();
+            
+            if(noOfFilesUploaded == 2) {
+                System.out.println("Status: All Files are uploaded successfully.");
+            } else {
+                System.out.println("Status: Files are not uploaded. Count: " + noOfFilesUploaded);
+            }
+            
+            // Validation 2: File names matching
+            String fileName1 = uploadedFiles.get(0).getText();
+            String fileName2 = uploadedFiles.get(1).getText();
+            
+            if(fileName1.equals("Test1.txt") && fileName2.equals("Test2.txt")) {
+                System.out.println("Validation: File names matching correctly.");
+            } else {
+                System.out.println("Validation: File names mismatch!");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Hamesha quit karein taaki Docker session release ho jaye
+            driver.quit();
+        }
+    }
 }

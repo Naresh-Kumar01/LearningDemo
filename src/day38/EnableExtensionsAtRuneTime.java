a34cf06e3b082baf1bf6c522eb5062ddd6fccf98
Naresh-Kumar01 <nareshsofttechh@gmail.com>
2026-04-29 09:42:33 -0700

    Added Day39 Selenium automation scripts

--- src/day38/EnableExtensionsAtRuneTime.java
+++ src/day38/EnableExtensionsAtRuneTime.java
@@ -1,5 +1,25 @@
 package day38;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class EnableExtensionsAtRuneTime {
+public static void main(String[] args) {
+		
+		ChromeOptions options=new ChromeOptions();
+		
+		File file=new File("C:\\Automation\\automationFiles\\crx files\\SelectorsHub.crx");
+		
+		options.addExtensions(file);
+	
+	
+		WebDriver driver=new ChromeDriver(options);
+		
+		driver.get("https://text-compare.com/");
+	
+}
 
 }
